4cd52b872dccc0de59fa580954f4baac9dc00f0a
Uku Volke <ukuvolke@gmail.com>
2019-05-15 15:01:54 +0300

    parandatud

--- yl9.py
+++ yl9.py
@@ -1,12 +1,12 @@
-print(" Tere, see on kolmnurga tüübi arvutamise masin.")
+print("Tere, see on kolmnurga tüübi arvutamise masin.")
 a=float(input("sisesta külje a pikkus: "))
 b=float(input("sisesta külje b pikkus: "))
 c=float(input("sisesta külje c pikkus: "))
 if (a==b) and (b==c):
     print("Kolmnurk on võrdkülgne")
-elif (a==b) or (b==c) or (c==a) and not (((a+b)<c) or ((b+c)<a) or ((c+a)<b)):
-    print("Kolmnurk on võrdhaarne")
 elif (((a+b)<c) or ((b+c)<a) or ((c+a)<b)):
     print("Sellist kolmurka pole olemas")
+elif (a==b) or (b==c) or (c==a) and not (((a+b)<c) or ((b+c)<a) or ((c+a)<b)):
+    print("Kolmnurk on võrdhaarne")
 else:
     print("Kolmnurk on erikülgne ")
